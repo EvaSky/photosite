9a53cb853e936606af23ebb80fd036f9f64ea087
Volha Shakhrai <Volha_Shakhrai@epam.com>
2017-11-11 15:33:50 +0300

    added ddl for record entity

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS records;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE records
+(
+  id        INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  date      DATE NOT NULL,
+  text      VARCHAR NOT NULL,
+);
